a0518403958f12c01c8a4e4de1062e4cd3500606
Dev <devkumarpatel20@gnu.ac.in>
2022-09-20 18:07:53 +0530

    Practical 5

--- app/src/main/java/com/example/mad_practical5_20012011083/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical5_20012011083/MainActivity.kt
@@ -3,11 +3,25 @@ package com.example.mad_practical5_20012011083
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var playbtn : FloatingActionButton
+    private lateinit var stopbtn : FloatingActionButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        playbtn = findViewById(R.id.btn_play)
+        playbtn.setOnClickListener(View.OnClickListener {
+            play()
+        })
+
+        stopbtn = findViewById(R.id.btn_stop)
+        stopbtn.setOnClickListener(View.OnClickListener {
+            stop()
+        })
     }
 
     fun play() {
